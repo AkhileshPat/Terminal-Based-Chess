2d300ed843d9c30376d82474aeb887546153e1f8
AkhileshPat <akpat2005@gmail.com>
2026-01-02 22:49:16 +0530

    Store The Very Basic Logic For Moving Indices In A Grid (2D Array)

--- Actual-Shit/0.1 board.py
+++ Actual-Shit/0.1 board.py
@@ -13,24 +13,28 @@ board = [
 
 F_Row = int(input("From Row : \n"))
 F_Col = int(input("From Col : \n"))
-board [F_Row-1] [F_Col-1] = "♚"
 
 # Here replace F_Row and F_Col with "square_to_position.py" output values
 
 
 print (board)
-
-
-T_Row = int(input("To Row : \n"))
-T_Col = int(input("To Col : \n"))
     
 # Here replace T_Row and T_Col with "square_to_position.py" output values
 
 
+T_Row = int(input("To Row : \n"))
+T_Col = int(input("To Col : \n"))
+
+
 def swapPOS(T_Row, T_Col) :
-    board [T_Row-1][T_Col-1] = "♚"
-    board [F_Row-1] [F_Col-1] = " "
-    # print (board)
+        
+        if board [F_Row-1] [F_Col-1] == "" :
+            return 
+        
+        else : 
+            board [T_Row-1][T_Col-1] = board [F_Row-1] [F_Col-1]
+            board [F_Row-1] [F_Col-1] = " "
+        
+        print (board)
 
-
-swapPOS(T_Row, T_Col)
+swapPOS(T_Row, T_Col) 
